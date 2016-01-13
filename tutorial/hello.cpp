aa674ae58d5ceafdba9f338c9bf8d82d2a0ee12f
Yuta Sugii <sugii@nowhere.co.jp>
2016-01-13 10:42:48 +0900

    💣 removed files

--- tutorial/hello.cpp
+++
@@ -1,8 +0,0 @@
-//hello.cpp
-
-#include <iostream>
-using namespace std;
-
-int main(){
-	cout << "hello" << endl;
-}
--- tutorial/nenrei.cpp
+++
@@ -1,9 +0,0 @@
-//nenrei.cpp
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "I am " << 43 << " old." << endl;
-}
